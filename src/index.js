d26c1a81fb390eac5568ab1e839c079eeb974a36
akramliam <130105833+akramliam@users.noreply.github.com>
2023-05-14 08:10:36 +0430

    fix forecast Days and show them

--- src/index.js
+++ src/index.js
@@ -24,25 +24,41 @@ if (minutes < 10) {
 pDate.innerHTML = `${days} ${hour}:${minutes}`;
 ///////
 
+function formatDay(timestamp){
+  let date= new Date(timestamp*1000);
+  let day=date.getDay();
+  let days=["Sun","Mon","Tue","Wed","Thu","Fri","Sat"];
+  return days[day];
+}
+
 function dailyForecast(response){
   console.log("dailyForecast function");
 
-  console.log(response.data.daily);
+  let forecastDays = response.data.daily;
+
 let forecastElement=document.querySelector("#forecast");
 let forecastHTML=`<div class="row">`;
-let days=["Sat","Sun","Mon","Tue","Wed"];
-days.forEach(function(day){
+//let days=["Sun","Mon","Tue","Wed"];
+
+forecastDays.forEach(function(forecastDay, index){
+  if(index<5){
   forecastHTML=forecastHTML+`<div class="col-2">
   <div class="weather-forecast-date">
-    ${day}
+  
+    ${formatDay(forecastDay.dt)}
   </div>
-<img src="images/sun.png" alt="sun" class="pic" alt="" width="45" />
+  <img
+  src="http://openweathermap.org/img/wn/${forecastDay.weather[0].icon}@2x.png"
+  alt=""
+  width="42"
+/>
   <div class="weather-forecast-temperatures">
-    <span class="weather-forecast-temperature-max">19°</span>
-    <span class="weather-forecast-temperature-min">14°</span>
+    <span class="weather-forecast-temperature-max">${Math.round(forecastDay.temp.max)}°</span>
+    <span class="weather-forecast-temperature-min">${Math.round(forecastDay.temp.min)}°</span>
 
   </div>
 </div>`
+}
 });
 forecastHTML=forecastHTML+`</div>`;
 forecastElement.innerHTML=forecastHTML;
